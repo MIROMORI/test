f70fc6e418b79ed36a5c68d7b27685b8c7db70e0
Matheus Aroxa <matheusaroxa13@gmail.com>
2024-11-01 21:09:25 -0300

    v2

--- src/main/java/org/miromorii/springapp/entity/Livro.java
+++ src/main/java/org/miromorii/springapp/entity/Livro.java
@@ -1,5 +1,6 @@
 package org.miromorii.springapp.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,13 +8,23 @@ import lombok.*;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tbl_livros")
 public class Livro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long isbn;
+
+    @Column
     private String titulo;
+    @Column
     private Integer edicao;
+    @Column
     private String autor;
+    @Column
     private Categoria categoria;
+    @Column
     private Modelo modelo;
 
 }
